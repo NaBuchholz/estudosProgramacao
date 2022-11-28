e7154676ff72bca25a9c81b1f88c584ab31c0c55
NaBuchholz <nathaliabuchh@hotmail.com>
2022-11-28 17:05:40 -0300

    Add resolução exercicios string

--- labenuDevFullStack/modulo1Fundamentos/exercícios/strings-exercicio/exercicio2/script.js
+++ labenuDevFullStack/modulo1Fundamentos/exercícios/strings-exercicio/exercicio2/script.js
@@ -1,1 +1,23 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+/*
+A partir dela, execute os passos abaixo:
+
+a) Remova o excesso de espaços no final da string;
+
+b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+
+c) Substitua os traços `________` por “sticioso”.
+*/
+
+let novaString = minhaString.trim().replaceAll('________', 'sticioso');
+
+console.log(novaString);
+
+novaString = novaString.replaceAll(" ","")
+
+// console.log(novaString);
+
+console.log(
+	`Quantidade de caracteres vazios\nAntes: ${minhaString.length}\nDepois: ${novaString.length}`
+);
--- labenuDevFullStack/modulo1Fundamentos/exercícios/strings-exercicio/exercicio3/script.js
+++ labenuDevFullStack/modulo1Fundamentos/exercícios/strings-exercicio/exercicio3/script.js
@@ -1,1 +1,36 @@
-//Crie a const para a frase aqui
+/* A partir da frase a seguir, faça o que se pede
+
+```
+Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+```
+
+a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo); */
+
+let frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`;
+
+//b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+
+const troca = frase.replace('verde', 'rosa').replace('azul', 'laranja');
+
+//c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+
+const verificacao = [troca.includes('verde'), troca.includes('laranja')];
+
+console.log(
+	`A nova frase inclui rosa? ${verificacao[0]}\nA nova frase inclui laranja? ${verificacao[1]}`
+);
+
+//**EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+
+/* um devaneio
+frase =
+	'Jorge tem uma casa verde e com portão azul, com os dizeres: ' +
+	'"' +
+	'BOAS VINDAS,' +
+	' mas não deixe o gato sair' +
+	'"';
+ */
+
+frase = frase.replace('mas não deixe o gato sair', 'MAS NÃO DEIXE O GATO SAIR');
+
+// console.log(frase);
