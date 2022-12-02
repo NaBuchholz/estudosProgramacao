2c11da5296ac3739ea47392260d756665ab87275
NaBuchholz <nathaliabuchh@hotmail.com>
2022-12-02 19:36:47 -0300

    Add speed test exercise resolution

--- onebitcode/exerciciosJavascript/ex002/ex002.js
+++ onebitcode/exerciciosJavascript/ex002/ex002.js
@@ -3,6 +3,29 @@ Teste de Velocidade
 
 Escreva um programa em javascript que permita inserir o nome e a velocidade de dois veículos e exiba na tela uma mensagem dizendo qual dos dois é mais rápido (ou que as velocidades são iguais se este for o caso)
 */
+const nameCar1 = prompt('Digite o nome do primeiro carro');
+const speedCar1 = +prompt('Digite a velocidade de ' + nameCar1);
+const nameCar2 = prompt('Digite o nome do segundo carro');
+const speedCar2 = +prompt('Digite a velocidade de ' + nameCar2);
+
+if (speedCar1 > speedCar2) {
+	alert(
+		`${nameCar1} tem a velocidade maior que ${nameCar2} por ${
+			speedCar1 - speedCar2
+		} km`
+	);
+} else if (speedCar1 === speedCar2) {
+	alert(`As velocidades de ${nameCar1} e ${nameCar2} são iguais!`);
+} else if (speedCar1 < speedCar2) {
+	alert(
+		`${nameCar1} tem a velocidade menor que ${nameCar2} por ${
+			speedCar2 - speedCar1
+		} km`
+	);
+} else {
+	alert('Dados insuficientes, tente novamente');
+}
+
 
 /*Cálculo de Dano
 
