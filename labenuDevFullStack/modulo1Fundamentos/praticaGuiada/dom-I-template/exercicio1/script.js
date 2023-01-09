78b5a3c531312c30de2786ff3b7689da49d63557
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-09 11:53:11 -0300

    resolve prática guiada DOM I

---
+++ labenuDevFullStack/modulo1Fundamentos/praticaGuiada/dom-I-template/exercicio1/script.js
@@ -0,0 +1,19 @@
+const sapo = document.getElementById('sapo');
+sapo.innerHTML = 'O sapo não lava o pé';
+console.log(sapo);
+
+const evidencias = document.getElementById('poema');
+evidencias.innerHTML += ' evidências';
+console.log(evidencias);
+
+const sabedoria = document.getElementById('sabedoria');
+sabedoria.innerHTML = 'Mais vale um pássaro na mão do que dois voando';
+console.log(sabedoria);
+
+const borboleta = document.getElementById('borboleta');
+borboleta.innerHTML = 'Poti-poti, perna de pau, olho de vidro e nariz de pica-pau';
+console.log(borboleta);
+
+const roberto = document.getElementById('roberto');
+roberto.innerHTML = 'Se chorei ou se sorri, o importante é que emoções eu vivi';
+console.log(roberto);
