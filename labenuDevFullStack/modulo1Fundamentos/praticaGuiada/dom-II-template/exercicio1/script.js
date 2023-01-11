1d288bfc48b245695e2237e136f7ef100cb517f7
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-11 16:44:39 -0300

    add DOM II exercices

---
+++ labenuDevFullStack/modulo1Fundamentos/praticaGuiada/dom-II-template/exercicio1/script.js
@@ -0,0 +1,10 @@
+const lista = document.getElementById('lista');
+
+const addNewItem = () => {
+	const newItem = document.createElement('li');
+	newItem.innerHTML = 'Item 0';
+	lista.insertAdjacentElement('afterbegin', newItem);
+	const novoItem = document.createElement(li);
+	const textoNovoItem = document.createTextNode('Item 5');
+	novoItem.appendChild();
+};
