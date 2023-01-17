7e8c90db6dd4044735d90ef52e88a21f6b351a9c
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-17 15:38:40 -0300

    resolução componentização

---
+++ labenuDevFullStack/modulo2FrontEnd/exercicios/componentes-exercicios/src/components/InfosUsuario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../img/logo.png';
+const InfosUsuario = () => {
+	return (
+		<div className="infos-usuario">
+			<img src={logo} width="8px" height="8px" />
+			<p>Nome Usuário</p>
+		</div>
+	);
+};
+export default InfosUsuario;
