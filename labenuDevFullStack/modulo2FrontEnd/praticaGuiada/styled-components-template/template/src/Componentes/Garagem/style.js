63a3d4e20c2e856288e032482fe395aa1cb408c0
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-19 12:47:44 -0300

    update projeto-intro-web

---
+++ labenuDevFullStack/modulo2FrontEnd/praticaGuiada/styled-components-template/template/src/Componentes/Garagem/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+export const Botao = styled.button`
+	border: none;
+	padding: 8px;
+	background-color: orange;
+	margin: 10px 0;
+
+	:hover {
+		background-color: orangered;
+	}
+`;
