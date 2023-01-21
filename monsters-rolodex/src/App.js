5e1f6eaca670a770cec8dce13a43729330d896a3
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-21 09:48:35 -0300

    add key concept in hardcode monsters

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,14 +5,18 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			monsters: [{ name: 'Linda' }, { name: 'Frank' }, { name: 'Jacky' }],
+			monsters: [
+				{ name: 'Linda', id: '134e44' },
+				{ name: 'Frank', id: '132e98' },
+				{ name: 'Jacky', id: '124y76' },
+			],
 		};
 	}
 	render() {
 		return (
 			<div className="App">
 				{this.state.monsters.map((monster) => {
-					return <h1>{monster.name}</h1>;
+					return <h1 key={monster.id}>{monster.name}</h1>;
 				})}
 			</div>
 		);
