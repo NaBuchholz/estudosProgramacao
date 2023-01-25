2ffbb1602287b3c69797b3674a9f9f5a8b097ddd
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-24 21:24:48 -0300

    exercics inputs

---
+++ labenuDevFullStack/modulo2FrontEnd/exercicios/inputs-controlados-exercicios/src/Components/NameForm/NameForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Form, Input } from '../MainPage/styles';
+
+const NameForm = (props) => {
+	return (
+		<Form>
+			<label>
+				Nome:
+				<Input
+					placeholder="Nome"
+					value={props.name}
+					onChange={props.onChangeName}
+					minLength={10}
+					maxLength={30}
+				/>
+			</label>
+			<label>
+				Idade:
+				<Input
+					placeholder="Idade"
+					value={props.age}
+					onChange={props.onChangeAge}
+				/>
+			</label>
+			<label>
+				E-mail:
+				<Input
+					placeholder="usuario@usuario.com"
+					value={props.email}
+					onChange={props.onChangeEmail}
+					pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+					required
+				/>
+			</label>
+			<label>
+				Confirmação de e-mail:
+				<Input
+					placeholder="usuario@usuario.com"
+					value={props.email}
+					onChange={props.onChangeEmail}
+					pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+					required
+				/>
+			</label>
+			<button onClick={props.sendData}>Enviar dados</button>
+		</Form>
+	);
+};
+
+export default NameForm;
