ce8c07c12eba9d62d133e8f8b26b1c446e149e3a
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-30 08:40:24 -0300

    install vite-plugin-svgr

--- udemy/ztm-ecommerce-store/src/routes/navgationBar/index.jsx
+++ udemy/ztm-ecommerce-store/src/routes/navgationBar/index.jsx
@@ -1,15 +1,22 @@
 import React, { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 const NavigationBar = () => {
 	return (
 		<Fragment>
 			<div className="navigation">
-				<div>logo</div>
+				<Link
+					className="logo-container"
+					to="#">
+					<CrwnLogo className="logo" />
+				</Link>
 				<div className="nav-links-container">
 					<Link
 						className="nav-link"
-						to=""></Link>
+						to="#">
+						Shop
+					</Link>
 				</div>
 			</div>
 			<Outlet />
