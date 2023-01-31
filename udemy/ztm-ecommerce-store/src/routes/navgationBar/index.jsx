780a958f15fc2e591b7b37fe3475bfdd85588311
NaBuchholz <nathaliabuchh@hotmail.com>
2023-01-31 07:01:59 -0300

    Update scss navbar

--- udemy/ztm-ecommerce-store/src/routes/navgationBar/index.jsx
+++ udemy/ztm-ecommerce-store/src/routes/navgationBar/index.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './styles.scss';
 
 const NavigationBar = () => {
 	return (
